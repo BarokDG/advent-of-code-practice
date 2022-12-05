909bd0be252c325b2c78dbce565ed8a7ebec0b4e
BarokDG <it.barok.dagim@gmail.com>
2022-12-05 10:17:19 +0300

    Add basic timer functionality

--- 2021/CSS/Challenge #1/index.js
+++ 2021/CSS/Challenge #1/index.js
@@ -1,1 +1,36 @@
 "use strict";
+
+const startButton = document.getElementById("start");
+const settingsButton = document.getElementById("settings");
+
+const timerOutput = document.getElementById("output");
+
+startButton.addEventListener("click", handleStartButtonClick);
+settingsButton.addEventListener("click", handleSettingsButtonClick);
+
+function handleStartButtonClick() {
+  startButton.textContent =
+    startButton.textContent === "Start" ? "Stop" : "Start";
+
+  startTimer();
+}
+
+function handleSettingsButtonClick() {}
+
+function startTimer() {
+  let timerStartTime = new Date();
+  let timerEndTime = new Date();
+  // timerEndTime.setMinutes(timerStartTime.getMinutes());
+  timerEndTime.setSeconds(timerStartTime.getSeconds() + 10);
+
+  let i = 0;
+  let timerID = setInterval(() => {
+    let timeRemaining = new Date(timerEndTime - Date.now());
+
+    timerOutput.innerHTML = `${timeRemaining.getMinutes()}:${timeRemaining.getSeconds()}`;
+
+    if (timerEndTime.getTime() <= Date.now()) {
+      clearInterval(timerID);
+    }
+  }, 1000);
+}
