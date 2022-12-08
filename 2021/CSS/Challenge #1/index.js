3041e35d915a84fb2edf55e7734b80afeff10afd
BarokDG <it.barok.dagim@gmail.com>
2022-12-08 22:44:58 +0300

    Add ring animation, update timer logic

--- 2021/CSS/Challenge #1/index.js
+++ 2021/CSS/Challenge #1/index.js
@@ -4,8 +4,68 @@ const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const settingsButton = document.getElementById("settings");
 
+const timerWrapper = document.querySelector(".timer-wrapper");
 const timerOutput = document.getElementById("output");
 
 startButton.addEventListener("click", handleStartButtonClick);
 stopButton.addEventListener("click", handleStopButtonClick);
-settingsButton.addEventListener("click", handleSettingsButtonClick);
+
+let timerID;
+let seconds = 0.1 * 60;
+let total = seconds;
+let baseDegreeIncrement = 360 / seconds;
+let timerDefaultDuration = 15; // 15 mins
+
+function handleStartButtonClick() {
+  toggleStartButtonVisibility();
+  toggleStopButtonVisibility();
+
+  startTimer();
+}
+
+function animateOuterRing() {
+  let currentDegree = baseDegreeIncrement * (total - seconds);
+  timerWrapper.style.background = `conic-gradient(var(--secondary) ${currentDegree}deg, #000 ${currentDegree}deg)`;
+}
+
+function handleStopButtonClick() {
+  toggleStartButtonVisibility();
+  toggleStopButtonVisibility();
+
+  stopTimer();
+}
+
+function toggleStartButtonVisibility() {
+  startButton.classList.toggle("hidden");
+}
+
+function toggleStopButtonVisibility() {
+  stopButton.classList.toggle("hidden");
+}
+
+function startTimer() {
+  timerID = setInterval(() => {
+    seconds--;
+    timerOutput.textContent = formatTime(seconds);
+    animateOuterRing();
+
+    if (seconds === 0) {
+      timerWrapper.style.background = "var(--tertiary)";
+      clearInterval(timerID);
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timerID);
+  timerOutput.textContent = formatTime(seconds);
+}
+
+function formatTime(timeRemainingInSeconds) {
+  let minutes = Math.trunc(timeRemainingInSeconds / 60);
+  let seconds = timeRemainingInSeconds % 60;
+
+  return `${minutes < 10 ? "0" + minutes : minutes}:${
+    seconds < 10 ? "0" + seconds : seconds
+  }`;
+}
