cfd2296c63f86526d984a0cf8630f9ae6ea409b8
BarokDG <it.barok.dagim@gmail.com>
2022-12-08 21:54:17 +0300

    clear index.js

--- 2021/CSS/Challenge #1/index.js
+++ 2021/CSS/Challenge #1/index.js
@@ -1,36 +1,11 @@
 "use strict";
 
 const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
 const settingsButton = document.getElementById("settings");
 
 const timerOutput = document.getElementById("output");
 
 startButton.addEventListener("click", handleStartButtonClick);
+stopButton.addEventListener("click", handleStopButtonClick);
 settingsButton.addEventListener("click", handleSettingsButtonClick);
-
-function handleStartButtonClick() {
-  startButton.textContent =
-    startButton.textContent === "Start" ? "Stop" : "Start";
-
-  startTimer();
-}
-
-function handleSettingsButtonClick() {}
-
-function startTimer() {
-  let timerStartTime = new Date();
-  let timerEndTime = new Date();
-  // timerEndTime.setMinutes(timerStartTime.getMinutes());
-  timerEndTime.setSeconds(timerStartTime.getSeconds() + 10);
-
-  let i = 0;
-  let timerID = setInterval(() => {
-    let timeRemaining = new Date(timerEndTime - Date.now());
-
-    timerOutput.innerHTML = `${timeRemaining.getMinutes()}:${timeRemaining.getSeconds()}`;
-
-    if (timerEndTime.getTime() <= Date.now()) {
-      clearInterval(timerID);
-    }
-  }, 1000);
-}
